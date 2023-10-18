0c8fb8e4f743792a2e65da7a7565e11e0f692dd3
dzikibeduin <olszowy.patryk@gmail.com>
2023-10-18 07:54:36 +0200

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { WalksModule } from './modules/walks/walks.module';
-import { PlatformAccessModule } from './modules/platform-access/platform-access.module';
 
 @Module({
-  imports: [WalksModule, PlatformAccessModule],
+  imports: [WalksModule],
   controllers: [],
   providers: [],
 })
--- src/modules/walks/walks.module.ts
+++ src/modules/walks/walks.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { WalksController } from './api/walks.controller';
+import { MongooseModule, SchemaFactory } from '@nestjs/mongoose';
+import { WalkSchema } from './infra/walk.schema';
+import { WalkFactory } from './infra/walk.factory';
+import { WalkDtoRepository } from './infra/walk-dto.repository';
+import { WalkEntityRepository } from './infra/walk-entity.repository';
+import { WalkSchemaFactory } from './infra/walk-schema.factory';
+import { CreateWalkCommand } from './app/commands/create-walk/create-walk.command';
+import { WalkCreatedEvent } from './core/walk/events/walk-created.event';
+import { GetDailyWalksQuery } from './app/queries/get-daily-walks/get-daily-walks.query';
 
 @Module({
-  imports: [CqrsModule],
+  imports: [
+    CqrsModule,
+    MongooseModule.forFeature([
+      {
+        name: WalkSchema.name,
+        schema: SchemaFactory.createForClass(WalkSchema),
+      },
+    ]),
+  ],
   controllers: [WalksController],
-  providers: [],
+  providers: [
+    WalkFactory,
+    WalkDtoRepository,
+    WalkEntityRepository,
+    WalkSchemaFactory,
+    CreateWalkCommand,
+    WalkCreatedEvent,
+    GetDailyWalksQuery,
+  ],
 })
 export class WalksModule {}
